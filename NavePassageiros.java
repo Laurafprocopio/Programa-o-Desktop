3e8d96ac78cf6dbac043c1ceb8ef1c62752be03e
Laurafprocopio <161400067+Laurafprocopio@users.noreply.github.com>
2024-03-14 20:17:53 -0400

    Update NavePassageiros.java

--- NavePassageiros.java
+++ NavePassageiros.java
@@ -1,5 +1,4 @@
 
-
 public class NavePassageiros extends NaveEspacial{
 
 	int numeropassageiros;
@@ -10,36 +9,14 @@ public class NavePassageiros extends NaveEspacial{
 		this.numeropassageiros = numeropassageiros;
 	}
 
-	public void acelerar(double velocidade) {
-		
-	}
 	
-	public void desacelerar(double velocidade) {
-		
-	}
-	
-	public void embarcar(int passageiro) {
-		 System.out.println(passageiro + " passageiros embarcaram na nave de passageiros ");
-	}
 
 	@Override
 	public String toString() {
 		return "NavePassageiros [numeropassageiros=" + numeropassageiros + "]";
 	}
 
-	@Override
-	public void abastecer(double litros) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void viajar(double distancia) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
+	
 	protected void carregar(int i) {
 		// TODO Auto-generated method stub
 		
